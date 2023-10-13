e22c55ebf264e11f9ab4762bd572cef9ac5fc50b
piroshi1989 <hiroshi19890606@gmail.com>
2023-10-13 14:00:59 +0900

    create search.js

--- src/resources/views/index__search.blade.php
+++ src/resources/views/index__search.blade.php
@@ -34,22 +34,5 @@
 </div>
 
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script>
-  document.addEventListener("DOMContentLoaded", function() {
-    const filterForm = document.getElementById("form");
-
-    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
-    const selectBox = document.getElementById("sort");
-
-    checkboxes.forEach(function(checkbox) {
-      checkbox.addEventListener("change", function() {
-        form.submit();
-      });
-    });
-
-    selectBox.addEventListener("change", function() {
-      form.submit();
-    });
-  });
-</script>
+<script src="{{ asset('js/search.js') }}"></script>
 @endsection
